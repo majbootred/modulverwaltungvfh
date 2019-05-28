ace5a11387b799239c82700887839fcf8219f798
vka <verena.kauth@gmx.de>
2019-05-28 13:39:32 +0200

    model.py class Module hinzugefügt

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Module(models.Model):
+    MID = models.CharField(max_length=4, unique=True)
+    Name = models.CharField(max_length=50)
+    WPF = models.BooleanField(default=0)
+    SS = models.BooleanField(default=0)
+    WS = models.BooleanField(default=0)
+    Branch = models.CharField(max_length=4, null=True)

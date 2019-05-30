b0ef8f8e05d5afed4260853a79233c29ae5f8090
Marlen Jacob <maj@riseup.net>
2019-05-30 13:21:13 +0200

    add generic templates and assets, include django-bootstrap3

--- app/views.py
+++ app/views.py
@@ -8,6 +8,6 @@ from django.http import HttpResponse
 
 def index(request):
     all_entries = Module.objects.all()
-    return render(request, 'app/db_inhalt.html', {'all_entries': all_entries})
+    return render(request, 'app/index.html', {'all_entries': all_entries})
 
 
--- modulverwaltungvfh/settings.py
+++ modulverwaltungvfh/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app',
+    'bootstrap3'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'modulverwaltungvfh.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,8 +129,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+# for app specific files
 STATIC_URL = '/static/'
 
+# for generic files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "assets")
+]
+
+# for productive use this
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# collect all static files in this folder
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

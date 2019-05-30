55ab18f8bd90f5fffc973752db18871f898c7bd8
Marlen Jacob <maj@riseup.net>
2019-05-30 14:41:25 +0200

    change signup to register, change default language to germen

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', views.signup_view, name='signup')
+    path('register/', views.register_view, name='register')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,15 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 
-def signup_view(request):
-    # a new user submitted a form
+def register_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            #log user in
+            # TODO: log user in
             return redirect('app:index')
     else:
         form = UserCreationForm()
 
-    return render(request, 'accounts/signup.html', {'form': form})
+    return render(request, 'accounts/register.html', {'form': form})

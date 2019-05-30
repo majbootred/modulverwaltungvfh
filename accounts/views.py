9e3ff5d07a354c76665e65ad172d96805f183cdc
Marlen Jacob <maj@riseup.net>
2019-05-30 15:41:16 +0200

    add login functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 
 def register_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            # TODO: log user in
+            user = form.save()
+            login(request, user)
             return redirect('app:index')
     else:
         form = UserCreationForm()
@@ -17,7 +18,11 @@ def register_view(request):
 
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request.POST)
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('app:index')
     else:
         form = AuthenticationForm()
 

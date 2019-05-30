fe61d97c261f6f5e4f0bd161c71f56f33b3300a9
Marlen Jacob <maj@riseup.net>
2019-05-30 14:12:20 +0200

    add dummy register-form

---
+++ accounts/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+
+def signup_view(request):
+    form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form': form})
--- modulverwaltungvfh/urls.py
+++ modulverwaltungvfh/urls.py
@@ -19,8 +19,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", include('app.urls')),
     path('admin/', admin.site.urls),
+    path('', include('app.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
 
 # for serving statics

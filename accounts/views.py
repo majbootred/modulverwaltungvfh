623cce1d5179995e6c1a2a78c85ba8c326584d2f
Marlen Jacob <maj@riseup.net>
2019-05-30 15:24:18 +0200

    just some styling

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 def register_view(request):
@@ -13,3 +13,12 @@ def register_view(request):
         form = UserCreationForm()
 
     return render(request, 'accounts/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'accounts/login.html', {'form': form})

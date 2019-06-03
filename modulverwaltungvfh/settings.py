228865aada7d6d5997b0c55e7814d73f00c7abf1
Marlen Jacob <maj@riseup.net>
2019-06-03 17:51:50 +0200

    add db.json dump from verenas db

--- modulverwaltungvfh/settings.py
+++ modulverwaltungvfh/settings.py
@@ -77,12 +77,13 @@ WSGI_APPLICATION = 'modulverwaltungvfh.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+ }
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -91,7 +92,7 @@ DATABASES = {
         'PASSWORD': 'oos2019django',
         'HOST': 'modulverwaltungvfh.mysql.pythonanywhere-services.com',
     }
-}
+}'''
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators

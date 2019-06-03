4f769de34f335bb4aeca91991f1d216f87fea185
Marlen Jacob <maj@riseup.net>
2019-06-03 09:17:15 +0200

    fix merge conflicts

--- app/views.py
+++ app/views.py
@@ -1,16 +1,14 @@
-<<<<<<< HEAD
-from django.shortcuts import render, get_object_or_404
-=======
 from django.shortcuts import render, redirect
->>>>>>> 3e860d3a33988c3b2d1256bff5859f9f106862cd
 from .models import Module
 from .models import Prerequisite
 
 
 def index(request):
-<<<<<<< HEAD
-    all_entries = Module.objects.all()
-    return render(request, 'app/index.html', {'all_entries': all_entries})
+    if request.user.is_authenticated:
+        all_entries = Module.objects.all()
+        return render(request, 'app/index.html', {'all_entries': all_entries})
+    else:
+        return redirect('accounts:login')
 
 
 def modulelist(request):
@@ -21,10 +19,3 @@ def modulelist(request):
 def prereqlist(request, modul):
     prereqs = Prerequisite.objects.filter(module__MID=modul)
     return render(request, 'app/prereqlist.html', {'prereqs': prereqs, 'modul':modul})
-=======
-    if request.user.is_authenticated:
-        all_entries = Module.objects.all()
-        return render(request, 'app/index.html', {'all_entries': all_entries})
-    else:
-        return redirect('accounts:login')
->>>>>>> 3e860d3a33988c3b2d1256bff5859f9f106862cd

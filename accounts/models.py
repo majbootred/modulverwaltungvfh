04f09f9ae5ce0e9d575cac615fd98acdb0b0d4be
vka <verena.kauth@gmx.de>
2019-06-16 12:54:34 +0200

    Profil-Seite auf Deutsch umgestellt

--- accounts/models.py
+++ accounts/models.py
@@ -7,11 +7,11 @@ class Student(models.Model):
         ('MINF', 'Medieninformatik'),
         ('WINF', 'Wirtschaftsinformatik'),
     )
-    userid = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    firstname = models.CharField(max_length=30)
-    lastname = models.CharField(max_length=30)
-    discipline = models.CharField(max_length=4, choices=DISCIPLINES, default='MINF', null=False, blank=False)
-    startingSemester = models.CharField(max_length=7, null=False, blank=False)  # WS19/20, SS19
+    userid = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, verbose_name="Benutzername")
+    firstname = models.CharField(max_length=30, verbose_name="Vorname")
+    lastname = models.CharField(max_length=30, verbose_name="Nachname")
+    discipline = models.CharField(max_length=4, choices=DISCIPLINES, default='MINF', null=False, blank=False, verbose_name="Fachrichtung")
+    startingSemester = models.CharField(max_length=7, null=False, blank=False, verbose_name="Startsemester")  # WS19/20, SS19
 
     def __str__(self):
         return '{}'.format(self.userid)
